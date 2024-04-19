6f0e93a69aef5580307ebe8b68f3871a97beed3d
gwajoon <hengguojun25@gmail.com>
2024-04-19 12:50:31 +0800

    Change bin ultrasonic sensor outputs

--- bin_ultrasonic_microbit.js
+++ bin_ultrasonic_microbit.js
@@ -1,26 +1,11 @@
 radio.setGroup(7)
 
-let preRecyclableBinDistance = 0;
-let postRecyclableBinDistance = 0;
-let preGeneralBinDistance = 0;
-let postGeneralBinDistance = 0;
-
-basic.forever(function () {
-  recyclableBinDistance = grove.measureInCentimetersV2(DigitalPin.P0);
-  generalBinDistance = grove.measureInCentimetersV2(DigitalPin.P2);
-});
+let generalBinDistance = 0
+let recyclableBinDistance = 0
 
 radio.onReceivedString(function (receivedString) {
-  recyclableBinDistance = grove.measureInCentimetersV2(DigitalPin.P0);
-  generalBinDistance = grove.measureInCentimetersV2(DigitalPin.P2);
-  if (receivedString == "preprocessing") {
-    preRecyclableBinDistance = recyclableBinDistance
-    preGeneralBinDistance = generalBinDistance
-  } else if (receivedString == "postprocessing") {
-    postRecyclableBinDistance = recyclableBinDistance
-    postGeneralBinDistance = generalBinDistance
-    serial.writeLine("recyclable:" + [preRecyclableBinDistance, postRecyclableBinDistance]);
-    serial.writeLine("general:" + [preGeneralBinDistance, postGeneralBinDistance]);
-  }
+    recyclableBinDistance = grove.measureInCentimetersV2(DigitalPin.P2)
+    generalBinDistance = grove.measureInCentimetersV2(DigitalPin.P0)
+    serial.writeLine("recyclable," + recyclableBinDistance)
+    serial.writeLine("general," + generalBinDistance)
 })
-
